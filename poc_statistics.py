1f4f0ca57c92ad0a6d47fd41342b37bb8fd3f3ce
Moore <wangmengzju@gmail.com>
2017-11-05 21:41:44 +0800

    更新不计入手续费时的结算结果

--- poc_statistics.py
+++ poc_statistics.py
@@ -28,6 +28,8 @@ strptime, strftime = datetime.datetime.strptime, datetime.datetime.strftime
 datelist = rl.dateRange(startday_str, endday_str)
 datelist_possible = list(set(funds_net["date"].values.tolist()))
 datelist_possible.sort(key=funds_net["date"].values.tolist().index)
+datelist_possible_moneyfund = list(set(funds_profit["date"].values.tolist()))
+datelist_possible_moneyfund.sort(key=funds_profit["date"].values.tolist().index)
 
 
 def poc_sta(datepairs):
@@ -38,7 +40,7 @@ def poc_sta(datepairs):
         for startday_str_sta, endday_str_sta in datepairs:
             datelist_sta_temp = rl.dateRange(startday_str_sta, endday_str_sta)
             datelist_sta = [w for w in datelist_sta_temp if w.replace("-", "") in datelist_possible]
-            company_df1 = pd.read_csv(il.cwd + r"\result\\" + company_file + "_result_1.csv")
+            company_df1 = pd.read_csv(il.cwd + r"\result\\" + company_file + "_result_nofee.csv")
             company_df = company_df1.ix[:, 1:]
             if startday_str_sta not in company_df.columns:
                 company_df.insert(0, startday_str_sta, ini_money)
@@ -52,7 +54,185 @@ def poc_sta(datepairs):
             user_sta[startday_str_sta + "-" + endday_str_sta + "-year_rate"] = (
                 ((company_result_this_period.iloc[-1] - company_result_this_period.iloc[0]) /
                  company_result_this_period.iloc[0]) / (len(datelist_sta_temp) / 365))
+        user_sta.to_csv(il.cwd + r"\result\\" + company_file + "_sta_nofee.csv")
         print(user_sta)
+
+
+def getUserCombinationByDate(date, user_combination):
+    return_dic = {}
+    combination_dates_list = list(set(user_combination["date"].values.tolist()))
+    if date in combination_dates_list:
+        combination_dates_df = user_combination[user_combination["date"] == date]
+        for index2, row2 in combination_dates_df.iterrows():
+            # 根据该用户在当天的组合情况计算其总净值
+            fund_ticker = row2["ticker"]
+            # 基金编号
+            fund_percent = float(row2["percent"])
+            # 基金比例
+            return_dic[fund_ticker] = fund_percent
+        return return_dic
+    else:
+        combination_dates_list.append(date)
+        combination_dates_list.sort()
+        date_index = combination_dates_list.index(date)
+        if date_index == 0:
+            return False
+        else:
+            combination_date = combination_dates_list[date_index - 1]
+            combination_dates_df = user_combination[user_combination["date"] == combination_date]
+            for index2, row2 in combination_dates_df.iterrows():
+                # 根据该用户在当天的组合情况计算其总净值
+                fund_ticker = row2["ticker"]
+                # 基金编号
+                fund_percent = float(row2["percent"])
+                # 基金比例
+                return_dic[fund_ticker] = fund_percent
+            return return_dic
+
+
+def get_user_hold_by_date(date, user_combination, usermoney):
+    return_user_hold = {}
+    return_buy_money = {}
+    left_money = usermoney
+    user_combination_date = user_combination[user_combination["date"] == date]
+    for index2, row2 in user_combination_date.iterrows():
+        # 对该公司对该用户组合在当天日期内的每个配置情况循环买入基金
+        fund_ticker = row2["ticker"]
+        # 基金编号
+        fund_percent = float(row2["percent"])
+        # 基金比例
+        buymoney = float(usermoney) * float(fund_percent)
+        if fund_ticker in funds_net["ticker"].values.tolist():
+            if date in datelist_possible:
+                exchange_date = date
+            else:
+                datelist_possible_temp = datelist_possible.copy()
+                datelist_possible_temp.append(date)
+                datelist_possible_temp.sort()
+                date_index = datelist_possible_temp.index(date)
+                if date_index + 1 < len(datelist_possible_temp) - 1:
+                    exchange_date = datelist_possible_temp[date_index + 1]
+                else:
+                    exchange_date = "last-day"
+            if not exchange_date == "last-day":
+                fund_net = rl.getFundsNetNext_byTickerDate(fund_ticker, exchange_date.replace("-", ""),
+                                                           funds_net,
+                                                           "%Y%m%d")
+                return_user_hold[fund_ticker] = buymoney / fund_net
+                left_money = left_money - buymoney
+                return_buy_money[fund_ticker] = buymoney
+        elif fund_ticker in funds_profit["ticker"].values.tolist():
+            if date in datelist_possible_moneyfund:
+                exchange_date = date
+            else:
+                datelist_possible_temp = datelist_possible_moneyfund.copy()
+                datelist_possible_temp.append(date)
+                datelist_possible_temp.sort()
+                date_index = datelist_possible_temp.index(date)
+                if date_index + 1 < len(datelist_possible_temp) - 1:
+                    exchange_date = datelist_possible_temp[date_index + 1]
+                else:
+                    exchange_date = "last-day"
+            if not exchange_date == "last-day":
+                fund_profit = rl.getFundsNetNext_byTickerDate(fund_ticker, exchange_date.replace("-", ""),
+                                                              funds_profit,
+                                                              "%Y%m%d")
+                return_user_hold[fund_ticker] = float(fund_profit) * (buymoney / 10000) + buymoney
+                left_money = left_money - buymoney
+                return_buy_money[fund_ticker] = buymoney
+    return return_user_hold, return_buy_money, left_money
+
+
+def compute_value_by_date(user_funds_hold, buymoney, date):
+    return_money = 0
+    return_user_holde = user_funds_hold.copy()
+    for holdeticker, holdamount in user_funds_hold.items():
+        if holdeticker in funds_net["ticker"].values.tolist():
+            if date.replace("-", "") in datelist_possible:
+                value_date = date
+            else:
+                datelist_possible_temp = datelist_possible.copy()
+                datelist_possible_temp.append(date)
+                datelist_possible_temp.sort()
+                date_index = datelist_possible_temp.index(date)
+                if date_index - 1 > 0:
+                    value_date = datelist_possible_temp[date_index - 1]
+                else:
+                    value_date = "first-day"
+            if not value_date == "first-day":
+                fund_net = rl.getFundsNetNext_byTickerDate(holdeticker, value_date.replace("-", ""),
+                                                           funds_net,
+                                                           "%Y%m%d")
+                return_money = return_money + fund_net * holdamount
+            else:
+                return_money = return_money + buymoney[holdeticker]
+        elif holdeticker in funds_profit["ticker"].values.tolist():
+            if date.replace("-", "") in datelist_possible_moneyfund:
+                value_date = date
+            else:
+                datelist_possible_temp = datelist_possible_moneyfund.copy()
+                datelist_possible_temp.append(date)
+                datelist_possible_temp.sort()
+                date_index = datelist_possible_temp.index(date)
+                if date_index - 1 > 0:
+                    value_date = datelist_possible_temp[date_index - 1]
+                else:
+                    value_date = "first-day"
+            if not value_date == "first-day":
+                fund_net = rl.getFundsNetNext_byTickerDate(holdeticker, value_date.replace("-", ""),
+                                                           funds_profit,
+                                                           "%Y%m%d")
+                return_money = return_money + fund_net * (holdamount / 10000) + holdamount
+                return_user_holde[holdeticker] = fund_net * (holdamount / 10000) + holdamount
+            else:
+                return_money = return_money + buymoney[holdeticker]
+    return return_money, return_user_holde
+
+
+def poc_detail_compute_nofee():
+    '''
+        计算不同厂家给出的配置相应的每日净值，并输出为文件,不计算费率和赎回过程中的资金延迟
+    '''
+    for company_file in company_file_names:
+        # 对每一个公司给出的配置情况循环
+        company_df = il.getZS_Company_combination(il.cwd + r"\history_data\\" + company_file + ".csv")
+        company_detial = pd.DataFrame()
+        time_cost = 0.0
+        for index, row in users.iterrows():
+            start = time.clock()
+            print("计算第" + str(index) + "/" + str(len(users)) + "个用户.")
+            # 对每一个用户循环
+            userid = row["userid"]
+            leftusermoney = usermoney = float(row["moneyamount"]) * 10000
+            user_combination = company_df[company_df['userid'] == userid]
+            user_funds_hold = {}
+            user_marketcap = {}
+            buy_money = {}
+            last_change_date = ""
+            for date in datelist:
+                # print("当前回测日期为" + str(date) + ".")
+                # 对回测时间段内的每一个日期循环
+                if not bool(user_funds_hold):
+                    if date in user_combination["date"].values.tolist():
+                        user_marketcap[date] = usermoney
+                        user_funds_hold, buy_money, leftusermoney = get_user_hold_by_date(date, user_combination,
+                                                                                          usermoney)
+                    else:
+                        user_marketcap[date] = usermoney
+                elif date in user_combination["date"].values.tolist():
+                    marketcap_date, user_funds_hold = compute_value_by_date(user_funds_hold, buy_money, date.replace("-", ""))
+                    user_marketcap[date] = marketcap_date + leftusermoney
+                    user_funds_hold, buy_money, leftusermoney = get_user_hold_by_date(date, user_combination,
+                                                                                      marketcap_date)
+                else:
+                    marketcap_date, user_funds_hold = compute_value_by_date(user_funds_hold, buy_money, date.replace("-", ""))
+                    user_marketcap[date] = marketcap_date + leftusermoney
+            company_detial = company_detial.append(user_marketcap, ignore_index=True)
+            elapsed = (time.clock() - start)
+            time_cost += elapsed
+            print("Time used:", elapsed)
+            print("Time Left Estimated:", (time_cost / (int(index) + 1)) * len(users) - time_cost)
+        company_detial.to_csv(il.cwd + r"\result\\" + company_file + "_result_nofee.csv")
 
 
 def poc_detail_compute():
@@ -187,7 +367,7 @@ def poc_detail_compute():
                                                                                     funds_net, "%Y%m%d")
                                 fund_marketcap = holdamount * hold_fund_net
                             elif fund_ticker in funds_profit["ticker"].values.tolist():
-                                # 如果该基金是货币基金，则没有手续费，直接记录初次买入的金额
+                                # 如果该基金是货币基金，则没有手续费
                                 hold_fund_net = rl.getFundsNetBefore_byTickerDate(holdeticker, date.replace("-", ""),
                                                                                   funds_profit, "%Y%m%d")
                                 if hold_fund_net == 0.0:
@@ -256,3 +436,4 @@ if __name__ == '__main__':
     date_pairs = [("2017-07-01", "2017-07-31"), ("2017-08-01", "2017-08-31"), ("2017-09-01", "2017-09-30"),
                   ("2017-10-01", "2017-10-31"), ("2017-07-01", "2017-10-31")]
     poc_sta(date_pairs)
+    # poc_detail_compute_nofee()
